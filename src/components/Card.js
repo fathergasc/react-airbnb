08514f91203a63e0dc9b435e07f1bbd692146e7e
fathergasc <quaglioniandrea@gmail.com>
2022-12-02 14:12:58 +0100

    add: conditional online alert on card

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,10 +2,22 @@ import React from "react";
 import star from '../img/star.png'
 
 export default function Card(props) {
+
+    let alertText 
+    if (props.openSpots === 0) {
+        alertText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        alertText = "ONLINE"
+    }
+
     return (
         <div className="card">
+
         {/* conditional rendering of soldout alert */}
-        {props.openSpots === 0 && <div className="availability">SOLD OUT</div>}
+        {/* {props.openSpots === 0 && <div className="availability"> SOLD OUT </div>} */}
+
+        {/* conditional rendering of soldout/online alert */}
+        {alertText && <div className="availability"> { alertText } </div>}
             <img src={`img/${props.img}`} alt="" className="card-img"/>
             <div className="card-infos">
                 <img src={star} alt="" className="star-icon"/>
