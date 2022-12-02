5eb7d7de4f2ec1947cc3d9ab31213f4704a1957b
fathergasc <quaglioniandrea@gmail.com>
2022-12-02 13:30:58 +0100

    add: hardcoded props to card

---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+import React from "react";
+import star from '../img/star.png'
+
+export default function Card(props) {
+    return (
+        <div className="card">
+            <img src={`img/${props.img}`} alt="" className="card-img"/>
+            <div className="card-infos">
+                <img src={star} alt="" className="star-icon"/>
+                <span>{props.rating}</span>
+                <span className="gray-text">({props.reviewCount}) • </span>
+                <span className="gray-text">{props.country}</span>
+            </div>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span>  / person</p>
+        </div>
+    )
+}
